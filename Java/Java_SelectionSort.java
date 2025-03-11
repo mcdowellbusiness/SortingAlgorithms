690bc72ffa92aeef3e624a1d0c20f753f4eea95f
Amey Bahadkar <bahadkaramey@gmail.com>
2025-03-10 21:53:44 -0500

    updated

--- Java/Java_SelectionSort.java
+++ Java/Java_SelectionSort.java
@@ -3,34 +3,24 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Java_SelectionSort 
-{ 
+public class Java_SelectionSort {
 
     /**
      * Sorts an array of integers using the Selection Sort algorithm.
      *
      * @param arr The array to be sorted.
      */
-
-
-    public static void selectionSort(int[] arr) 
-{
+    public static void selectionSort(int[] arr) {
         int n = arr.length;
 
-        for (int i = 0; i < n - 1; i++) 
-{
+        for (int i = 0; i < n - 1; i++) {
             int min_idx = i;
-
-            for (int j = i + 1; j < n; j++) 
-{
-                if (arr[j] < arr[min_idx]) 
-{
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
                     min_idx = j;
                 }
             }
-
-            if (min_idx != i) 
-{
+            if (min_idx != i) {
                 int temp = arr[i];
                 arr[i] = arr[min_idx];
                 arr[min_idx] = temp;
@@ -38,17 +28,15 @@ public class Java_SelectionSort
         }
     }
 
-    public static void main(String[] args) 
-{
-        try 
-{
+    public static void main(String[] args) {
+        try {
             File file = new File("input.txt");
             Scanner scanner = new Scanner(file);
             String line = scanner.nextLine();
             String[] numbers = line.split(",");
             int[] arr = new int[numbers.length];
-            for (int i = 0; i < numbers.length; i++) 
-{
+
+            for (int i = 0; i < numbers.length; i++) {
                 arr[i] = Integer.parseInt(numbers[i].trim());
             }
             scanner.close();
@@ -57,14 +45,11 @@ public class Java_SelectionSort
             selectionSort(arr);
             System.out.println("Sorted array: " + Arrays.toString(arr));
 
-        } 
-      catch (FileNotFoundException e) 
-{
+        } catch (FileNotFoundException e) {
             System.out.println("Error: input.txt not found.");
-        } 
-      catch (NumberFormatException e) 
-{
+        } catch (NumberFormatException e) {
             System.out.println("Error: Invalid number in input.txt.");
         }
     }
 }
+
