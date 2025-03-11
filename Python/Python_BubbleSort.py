408ec669e4382bb9454e02c0d6f125d0e1e49954
Alyssa Gargano <agargano@pnw.edu>
2025-03-11 12:48:35 +0000

    Removed accidental online debugger link from code and updated function to describe how the algorithm sorts.

--- Python/Python_BubbleSort.py
+++ Python/Python_BubbleSort.py
@@ -1,8 +1,10 @@
+# Sorts a given array by repeatedly swapping two values
+# next to each other until every value has been sorted
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arhttps://www.onlinegdb.com/#editor_1r[j] > arr[j+1]:
+            if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 def read_input_file(filename):
